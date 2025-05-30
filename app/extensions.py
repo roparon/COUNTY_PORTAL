daa432760a4b1a12bde00c9e96d415a43a4228ea
Aron Rop <aaronrop40@gmail.com>
2025-05-30 10:33:41 +0300

    modified my extensions for importation

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_security import Security
+from flask_mail import Mail
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+security = Security()
+mail = Mail()
