ac39889a2b434b5fa5578551a0c8fa5847c54267
Aron Rop <aaronrop40@gmail.com>
2025-05-30 11:19:04 +0300

    modified my user. py file

--- app/models/user.py
+++ app/models/user.py
@@ -17,9 +17,14 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
     # Account status
     active = db.Column(db.Boolean, default=True)
+    confirmed_at = db.Column(db.Datetime())
+    created_at = db.Column(db.DateTime(), default=db.func.current_Timestamp())
     #flask-security required fields for tokens, sessions, password management
     fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
-
+    # Tracking fields
+    # TODO:
+    # Relationship
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
 
 
 class Role(db.Model):
