37d9c127ac78ed538b2ec856ecf167b7f07c02bd
Aron Rop <aaronrop40@gmail.com>
2025-05-30 10:41:24 +0300

    modified my extensions and added two more files

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
+from flask_security import SQLAlchemyUserDatastore
 from app.extensions import db
 from app.extensions import db, migrate
-from config import Config
+from config import Config, mail
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
 
     # Register the blueprints
     # Blueprints import
     from app.main.views import main_bp
     from app.api.routes import api_bp
-    
+
     # Register the API blueprint
     # Register the blueprint
     app.register_blueprint(main_bp)
