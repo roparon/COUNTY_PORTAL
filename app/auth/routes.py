b05ade9703a891d7279d19af55a7002e35a62e3b
Aron Rop <aaronrop40@gmail.com>
2025-06-05 16:42:11 +0300

    modified models register.html for intention errors

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -100,7 +100,7 @@ def edit_user(user_id):
     counties = County.query.filter_by(active=True).all()                      
     departments = Department.query.filter_by(active=True).all()               
                                                                                 
-    return render_template('auth/edit_user.html',                             
+    return render_template('auth/edith_user.html',                             
                             user=user,                                           
                             roles=roles,                                         
                             counties=counties,                                   
